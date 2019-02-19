b9d9edeb8c114c85a16577f3668e6ea58ae13f9f
lurume84 <lurume84@gmail.com>
2019-02-19 13:11:12 +0100

    u2f pipeline added for register and login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
 import { DialogService } from 'systelab-components/widgets/modal/dialog/dialog.service';
 import { ChangePasswordDialog } from './systelab-login/change-password-dialog.component';
+import { UserService } from './common/api/user.service';
+import { ApiGlobalsService } from './globals/globals.service';
+import u2fApi from 'u2f-api';
+import { User } from './common/model/user';
+import { MessagePopupService } from 'systelab-components/widgets/modal';
+import { LoginComponent } from './systelab-login/login.component';
 
 @Component({
 	selector:      'app-root',
@@ -28,30 +34,130 @@ export class AppComponent {
 	public isLoginActive = true;
 	public isLoading = false;
 
-	public constructor(protected dialogService: DialogService) {
+	@ViewChild('loginName') loginComponent:LoginComponent;
+	
+	public constructor(protected dialogService: DialogService, protected userService: UserService, protected apiGlobalsService: ApiGlobalsService, protected messagePopupService: MessagePopupService) {
 		this.currentForm = 'login';
 		this.txtUsername = 'Username';
 	}
 
-	public doLogin() {
+	public doLogin(event: any) {
 		console.log(this.userName + ' ' + this.password);
-		this.dialogService.showDialog(ChangePasswordDialog, ChangePasswordDialog.getParameters());
 		console.log('doLogin');
 		this.isLoading = true;
+		if (this.userName && this.password)
+		{
+			this.userService.login(this.userName, this.password)
+				.subscribe(
+					(response) => {
+						this.apiGlobalsService.bearer = response.headers.get('Authorization').replace("\\/", "/").replace("\\/", "/");
+						
+						var self = this;
+
+						u2fApi.isSupported().then( function( supported ) {
+							if ( supported )
+							{
+								console.log(self.messagePopupService.showInformationPopup("Performing U2F action", "Please touch the flashing U2F device now. You may be prompted to allow the site permission to access your security keys. After granting permission, the device will start to blink."));
+								
+								var request = JSON.parse(self.apiGlobalsService.bearer);
+
+								u2fApi.sign(request).then(function(response)
+								{
+									document.getElementsByTagName("modal-overlay")[0].remove();
+
+									var block: any = response;
+									block.user = self.userName;
+
+									self.userService.login_verify(JSON.stringify(block))
+									.subscribe(
+										(response2) => {
+											self.isLoading = false;
+											self.messagePopupService.showInformationPopup("Success","User has been correctly identified");
+										},
+										(error) => self.showError(error));
+								}).catch( function (e) {
+									document.getElementsByTagName("modal-overlay")[0].remove();
+									self.showError(e);
+								});
+							}
+							else
+							{
+								self.showError("u2f not supported");
+							}
+						} )
+						.catch(function (e) {
+							self.showError(e);
+						});
+					},
+					(error) => this.showError(error.message));
+		}
 	}
 
-	public doSignUp() {
+	public doSignUp(event: any) {
 		console.log(this.userName + ' ' + this.password + ' ' + this.name + ' ' + this.lastName + ' ' + this.email);
-		this.dialogService.showDialog(ChangePasswordDialog, ChangePasswordDialog.getParameters());
 		console.log('doSignUp');
 		this.isLoading = true;
+		if (this.userName && this.password)
+		{
+			let user : User = {id: "", surname: this.lastName, name: this.name, login: this.userName, password: this.password, role: 'USER'};
+
+			this.userService.create(user)
+				.subscribe(
+					(response) => {
+						//
+						this.apiGlobalsService.bearer = response.headers.get('Authorization').replace("\\/", "/").replace("\\/", "/");
+						
+						var self = this;
+
+						u2fApi.isSupported().then( function( supported ) {
+							if ( supported )
+							{
+								console.log(self.messagePopupService.showInformationPopup("Performing U2F action", "Please touch the flashing U2F device now. You may be prompted to allow the site permission to access your security keys. After granting permission, the device will start to blink."));
+								
+								var request = JSON.parse(self.apiGlobalsService.bearer);
+
+								u2fApi.register(request).then(function(response)
+								{
+									var block: any = response;
+									block.user = user.login;
+
+									document.getElementsByTagName("modal-overlay")[0].remove();
+									self.userService.create_verify(JSON.stringify(block))
+									.subscribe(
+										(response2) => {
+											self.isLoading = false;
+											self.loginComponent.goLogin();
+										},
+										(error) => self.showError(error));
+								}).catch( function (e) {
+									document.getElementsByTagName("modal-overlay")[0].remove();
+									self.showError(e);
+								});
+							}
+							else
+							{
+								self.showError("u2f not supported");
+							}
+						} )
+						.catch(function (e) {
+							self.showError(e);
+						});
+					},
+					(error) => this.showError(error.message));
+		}
 	}
 
-	public doRecovery() {
+	public doRecovery(event: any) {
 		console.log(this.userName);
 		this.dialogService.showDialog(ChangePasswordDialog, ChangePasswordDialog.getParameters());
 		console.log('doRecovery');
 		this.isLoading = true;
+	}
+
+	private showError(what: any){
+		this.isLoading = false;
+		this.messagePopupService.showErrorPopup("Error", what);
+		console.log(what);
 	}
 
 }
